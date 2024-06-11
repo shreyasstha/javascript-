121d8129f64d1072176ed9d7352fa391627bcaa7
Shreya shrestha <137528793+shreyasstha@users.noreply.github.com>
2024-06-11 16:51:45 +0545

    Add files via upload

---
+++ r.error_handling.js
@@ -0,0 +1,15 @@
+//const x =2;
+//nepal does not print
+
+let x = 2; //prints nepal as wells
+
+//*x = 7;  //error as x =2 so all the code are useless from here
+
+try {
+    x=7;
+    console.log("nepal"); //error so does not print this
+} catch (error) {
+    //console.log(error.message);
+    console.log("error here");
+}
+console.log("HI")
